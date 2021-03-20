5ccfd6cdc5c880b4572ddce08d7a88e4e2f6b3d2
blendthink <info@blendthink.dev>
2021-03-20 16:14:23 +0900

    initial commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by blendthink on 2021/03/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
