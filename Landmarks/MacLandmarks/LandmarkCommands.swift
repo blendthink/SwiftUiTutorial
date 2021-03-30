6c6e4ae5c97a4eea6a45040e460bf05ba0fbeca6
blendthink <info@blendthink.dev>
2021-03-30 09:17:20 +0900

    section5

---
+++ Landmarks/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by blendthink on 2021/03/30.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
